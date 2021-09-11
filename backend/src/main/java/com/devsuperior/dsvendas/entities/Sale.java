40de84fc15d017dd59bd8c7f12db844a15944c3c
Daniele Vilaca <danielevilaca27@gmail.com>
2021-09-11 14:21:01 -0300

    Domain model,database seed

---
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -0,0 +1,91 @@
+package com.devsuperior.dsvendas.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "tb_sales")
+public class Sale {
+	
+	 @Id  // chave primaria
+	 @GeneratedValue (strategy = GenerationType.IDENTITY)  //chave primaira sequencial autp_increment
+     private Long id;
+     private Integer visited; 
+     private Integer deals;
+     private double amount;
+     private LocalDate date;
+     
+     @ManyToOne   //muitos para um 
+     @JoinColumn(name="seller_id")  // mostrando a chave entrangeira do id do vendedor
+     private Seller seller;
+     
+     public Sale() {
+    	 
+     }
+
+	public Sale(Long id, Integer visited, Integer dealsInteger, double amount, LocalDate date, Seller seller) {
+		this.id = id;
+		this.visited = visited;
+		this.dealsInteger = dealsInteger;
+		this.amount = amount;
+		this.date = date;
+		this.seller = seller;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDealsInteger() {
+		return dealsInteger;
+	}
+
+	public void setDealsInteger(Integer dealsInteger) {
+		this.dealsInteger = dealsInteger;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public Seller getSeller() {
+		return seller;
+	}
+
+	public void setSeller(Seller seller) {
+		this.seller = seller;
+	}
+     
+     
+}
